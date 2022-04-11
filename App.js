565c3700eb559a1d0820846da4d8a1d2de646a60
Nandan N.K <nknandan@cet.ac.in>
2022-04-11 23:14:12 +0530

    hi

--- App.js
+++ App.js
@@ -1,45 +1,227 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {View, StyleSheet, StatusBar} from 'react-native';
+import RNLocation from 'react-native-location';
+import {Dimensions} from 'react-native';
+
 import {
   ViroARScene,
   ViroText,
   ViroConstants,
   ViroARSceneNavigator,
+  ViroAnimatedImage,
+  ViroImage,
+  ViroFlexView,
 } from '@viro-community/react-viro';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
-  function onInitialized(state, reason) {
-    // console.log('guncelleme', state, reason);
-    // if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
-    // } else if (state === ViroConstants.TRACKING_NONE) {
-    //   // Handle loss of tracking
-    // }
+const StartScreen = () => {
+  const [degree, setDegree] = useState('84');
+  const [degreeSign, setDegreeSign] = useState('o');
+  const [weatherType, setWeatherType] = useState('Mostly Cloudy');
+  const [devName, setDevName] = useState('Hexa-Devs');
+  const [place, setPlace] = useState('');
+  const [country, setCountry] = useState('India');
+  const [loading, setLoading] = useState(true);
+  const [iconWeather, setIconWeather] = useState(
+    'cdn.weatherapi.com/weather/64x64/night/143.png',
+  );
+  const [date, setDate] = useState('');
+  const [wind, setWind] = useState('');
+  const [humidity, setHumidity] = useState('');
+
+  RNLocation.configure({
+    distanceFilter: 5.0,
+  });
+  var loc, lat, long, weatherdata;
+  RNLocation.requestPermission({
+    ios: 'whenInUse',
+    android: {
+      detail: 'coarse',
+    },
+  }).then(granted => {
+    if (granted) {
+      RNLocation.getLatestLocation({timeout: 60000}).then(latestLocation => {
+        loc = latestLocation;
+        lat = loc.latitude;
+        long = loc.longitude;
+        apireq();
+      });
+    }
+  });
+  function apireq() {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=0721412bfd6b4d7c9e4153301221004&q=${lat},${long}&aqi=no`,
+    )
+      .then(res => res.json())
+      .then(data => {
+        weatherdata = data;
+        setPlace(weatherdata.location.name);
+        setDegree(Math.floor(weatherdata.current.temp_c).toString());
+        console.log(degree);
+        // setWeatherType(weatherdata.current.condition.text);
+        setIconWeather(`http:` + weatherdata.current.condition.icon);
+        setDate(weatherdata.location.localtime);
+        setWind(
+          `Wind Speed : ` + weatherdata.current.wind_kph.toString() + ' kph',
+        );
+        setHumidity(
+          `Humidity : ` + weatherdata.current.humidity.toString() + ' %',
+        );
+        setTimeout(function () {
+          setLoading(false);
+        }, 5000);
+      });
   }
 
+  function onInitialized(state, reason) {}
+
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+    <ViroARScene onTrackingUpdated={onInitialized} style={styles.box}>
+      {loading ? (
+        <ViroFlexView
+          height={1000}
+          width={1000}
+          position={[0, 0, -4]}
+          transformBehaviors={['billboard']}
+          backgroundColor={'#000000'}
+          style={styles.splashScreenbox}>
+          <ViroAnimatedImage
+            height={1}
+            width={1}
+            loop={true}
+            rotation={[0, 0, 0]}
+            position={[0, 0, -5]}
+            source={require('./assets/spinner.gif')}
+          />
+        </ViroFlexView>
+      ) : (
+        <ViroFlexView backgroundColor={'transparent'} position={[0, 0, 0]}>
+          <ViroText
+            text={degree}
+            scale={[1, 1, 1]}
+            position={[-0.3, 0.4, -2.8]}
+            outerStroke={{type: 'Outline', width: 2, color: '#000000'}}
+            style={styles.helloWorldTextStyle}
+          />
+          <ViroText
+            text={degreeSign}
+            scale={[1, 1, 1]}
+            position={[0.9, 0.2, -2.8]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.degreeSignStyle}
+          />
+          <ViroText
+            text="C"
+            scale={[1, 1, 1]}
+            position={[0.9, -0.2, -2.8]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.degreeFormatStyle}
+          />
+          <ViroText
+            text={weatherType}
+            scale={[1, 1, 1]}
+            position={[0.2, -0.9, -3]}
+            width={2}
+            height={1}
+            maxLines={1}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.weatherTypeStyle}
+          />
+          <ViroText
+            text={devName}
+            scale={[1, 1, 1]}
+            position={[-0, -3.1, -3]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.devNameStyle}
+          />
+          <ViroText
+            text={place}
+            scale={[1, 1, 1]}
+            position={[0.5, 0.9, -3.4]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.dateStyle}
+          />
+          <ViroText
+            text={country}
+            scale={[1, 1, 1]}
+            position={[1.05, 0.7, -3.4]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.dateStyle}
+          />
+          <ViroText
+            text={date}
+            scale={[1, 1, 1]}
+            position={[-0.9, 1.9, -3.4]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.dateStyle}
+          />
+          <ViroText
+            width={2}
+            height={1}
+            text={wind}
+            scale={[1, 1, 1]}
+            position={[1.8, -2.3, -3.4]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.detailsStyle}
+          />
+          <ViroText
+            width={2}
+            height={1}
+            text={humidity}
+            scale={[1, 1, 1]}
+            position={[1.8, -2.6, -3.4]}
+            outerStroke={{type: 'Outline', width: 1, color: '#000000'}}
+            style={styles.detailsStyle}
+          />
+
+          <ViroImage
+            height={0.7}
+            width={0.7}
+            position={[1, 1.9, -3]}
+            source={{
+              uri: iconWeather,
+            }}
+          />
+          <ViroImage
+            height={0.2}
+            width={0.2}
+            position={[1.14, 1, -3]}
+            source={require('./assets/location.png')}
+          />
+
+          <ViroImage
+            height={0.2}
+            width={0.2}
+            position={[0.6, -1.95, -3.4]}
+            source={require('./assets/wind.png')}
+          />
+          <ViroImage
+            height={0.2}
+            width={0.2}
+            position={[0.6, -2.25, -3.4]}
+            source={require('./assets/humidity.png')}
+          />
+        </ViroFlexView>
+      )}
     </ViroARScene>
   );
 };
 
 export default () => {
   return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
+    <View style={styles.container}>
+      {/* {loading1 ? <View style={styles.loadingScreen}></View> : <></>} */}
+      <StatusBar hidden />
+      <ViroARSceneNavigator
+        autofocus={true}
+        initialScene={{
+          scene: StartScreen,
+        }}
+        style={styles.f1}
+      />
+    </View>
   );
 };
 
@@ -47,9 +229,48 @@ var styles = StyleSheet.create({
   f1: {flex: 1},
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 30,
+    fontSize: 100,
     color: '#ffffff',
-    textAlignVertical: 'center',
+  },
+  degreeSignStyle: {
+    fontSize: 28,
+  },
+  degreeFormatStyle: {
+    fontSize: 50,
+    color: '#bbe7fe',
+  },
+  weatherTypeStyle: {
+    fontSize: 28,
+    color: '#EEDCE6',
+  },
+  dateStyle: {
+    fontSize: 18,
+    color: '#EEDCE6',
+  },
+  devNameStyle: {
     textAlign: 'center',
   },
+  detailsStyle: {
+    fontSize: 18,
+    color: '#D8D8F4',
+  },
+  container: {
+    flex: 1,
+  },
+  loadingScreen: {
+    height: windowHeight,
+    width: windowWidth,
+  },
+  LoadingText: {
+    color: 'white',
+  },
+  splashScreenbox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  fetchingTextStyle: {
+    marginRight: windowWidth/1.5,
+    fontSize: 42
+  }
 });
